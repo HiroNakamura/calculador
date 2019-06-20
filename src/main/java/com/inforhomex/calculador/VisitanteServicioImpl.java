e06f9f1407d9e8aaceb806130d2c8cda7721464a
Fer_Carraro <carraro.fernando@gmail.com>
2019-06-19 21:30:06 -0500

    Se edittaron varios archivos y se crearon Vistante, VisitanteServicio, etc.

--- src/main/java/com/inforhomex/calculador/VisitanteServicioImpl.java
+++ src/main/java/com/inforhomex/calculador/VisitanteServicioImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
-@Service("usuarioService")
+@Service("visitanteServicio")
 public class VisitanteServicioImpl implements VisitanteServicio {
 
         @Autowired
