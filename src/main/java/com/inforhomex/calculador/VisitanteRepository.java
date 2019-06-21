69439dd5f1d097d8abdc34c3486a1792df770424
Fer_Carraro <carraro.fernando@gmail.com>
2019-06-21 17:45:00 -0500

    Se eliminaron varios archivos inncesarios

--- src/main/java/com/inforhomex/calculador/VisitanteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.inforhomex.calculador;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("visitanteRepository")
-public interface VisitanteRepository extends JpaRepository<Visitante,Long>{
-
-}
--- src/main/java/com/inforhomex/calculador/VisitanteServicio.java
+++
@@ -1,7 +0,0 @@
-package com.inforhomex.calculador;
-
-import java.util.List;
-
-public interface VisitanteServicio {
-    public abstract List<Visitante> getAllVisitantes();
-}
