18aafe5b06331ab4b711d693da815c54e17d6bd2
Fer_Carraro <carraro.fernando@gmail.com>
2019-06-22 21:14:40 -0500

    Se eliminarion archivos innecesarios

--- src/main/java/com/inforhomex/calculador/controller/CursoController.java
+++ src/main/java/com/inforhomex/calculador/controller/CursoController.java
@@ -1,10 +1,19 @@
 package com.inforhomex.calculador.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/curso")
 public class CursoController{
 
-   public String index(){
-     return "index";
-   }
+	//http://localhost:8090/curso/index
+	@GetMapping("/index")
+	public String index(){
+		return "index";
+	}
 
 }
