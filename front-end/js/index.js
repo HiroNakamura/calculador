32a196ca8b03c1ab4c798cb55fb0413328745473
Fer_Carraro <carraro.fernando@gmail.com>
2019-06-24 12:13:19 -0500

    Cambios en front-end

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,8 +1,13 @@
 const h1 = document.getElementsByTagName("h1")[0];
 const p1 = document.getElementsByTagName("p")[0];
+const principal = document.querySelector("#principal");
+var idiomas = "";
+var valores = "";
+var mensajes = ""; 
 
 var Test = {
     init: function(){
+        
         fetch('http://localhost:8090/curso/index')
         .then(function(response) {
             return response.text();
@@ -11,6 +16,21 @@ var Test = {
             h1.textContent = texto.split(";")[1];
             p1.textContent = texto.split(";")[0]
         });  
+
+        fetch("http://localhost:8090/curso/mensajes")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(myJson){
+            for(var dato in myJson){
+                console.log("Idioma: ",myJson[dato].idioma);
+                console.log("Mensaje: ",myJson[dato].mensaje);
+                mensajes += "<br/>"+myJson[dato].idioma+ "  : "+myJson[dato].mensaje;
+            }
+            principal.innerHTML = mensajes;
+        });
+
+
     }
 };
 window.addEventListener("load",Test.init);
