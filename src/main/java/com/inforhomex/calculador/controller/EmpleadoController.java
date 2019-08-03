0dfacbf8ab2cdb26be3cce825bb1ebce7469b247
ARIEL <carraro.ariel@gmail.com>
2019-08-03 18:40:28 -0500

    Se edito EmpleadoController

--- src/main/java/com/inforhomex/calculador/controller/EmpleadoController.java
+++ src/main/java/com/inforhomex/calculador/controller/EmpleadoController.java
@@ -1,4 +1,4 @@
-package com.inforhomex.springforlotes.controller;
+package com.inforhomex.calculador.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
