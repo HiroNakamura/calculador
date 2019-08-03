cc34eeeaa09929720df05fe9c2686b233856379d
ARIEL <carraro.ariel@gmail.com>
2019-08-03 18:31:18 -0500

    Se creó entidad Empleado

---
+++ src/main/java/com/inforhomex/calculador/entity/Empleado.java
@@ -0,0 +1,88 @@
+package com.inforhomex.springforlotes.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity(name = "Empleado")
+@Table(name="empleados")
+public class Empleado implements Serializable{
+
+    private static final long serialVersionUID = 22L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "serial")
+    private Integer id;
+
+    private String nombre;
+    private String apellido;
+    
+    private Integer departamento;
+
+    private Double salario;
+
+    public Empleado(){}
+
+    public Empleado(Integer id, String nombre, String apellido, Integer departamento,Double salario){
+        super();
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.departamento = departamento;
+        this.salario = salario;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+   
+    public String getApellido() {
+        return apellido;
+    }
+
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Integer departamento) {
+        this.departamento = departamento;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString(){
+        return "Empleado{ id:"+id+",nombre:"+nombre+ ",apellido: "+apellido+",departamento: "+departamento+",salario: "+salario+"}";
+    }
+
+} 
