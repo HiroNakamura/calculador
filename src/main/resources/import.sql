990626bffbc1d5d3c42339b1417c8768939549cc
Fer_Carraro <carraro.fernando@gmail.com>
2019-08-09 12:18:14 -0500

    Se edito application

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,7 @@
-
-
 INSERT INTO public.notas(nombre,titulo,contenido) VALUES ('Umberto Eco','Sobre la esquizofrenia natural','Blah blah y blah');
 INSERT INTO public.notas(nombre,titulo,contenido) VALUES ('Inmanuel Kant','Tratado de logica distopica','En muchas palabras...');
 INSERT INTO public.notas(nombre,titulo,contenido) VALUES ('Hortencia Alcantara','Hablando de los sentimientos humanos','Creo que no escribire de esto');
 INSERT INTO public.notas(nombre,titulo,contenido) VALUES ('Tirsio Gomora','Informe sobre ciegos','Y exponiendo el asunto tenemos que las gallinas ponen muchos huevos');
-
 
 -- INSERT INTO public.empleados(nombre, apellido,depto, salario) VALUES('Juan','Lopez Lopez',1,23000.0);
 -- INSERT INTO public.empleados(nombre, apellido,depto, salario) VALUES('Mariana','Alcantara Villegas',2,25000.0);
