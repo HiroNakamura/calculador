76def73a6328723f82a2c8ee989ed76896146d5a
Fer_Carraro <carraro.fernando@gmail.com>
2019-08-09 12:10:06 -0500

    Se edito import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,5 +15,6 @@ INSERT INTO public.notas(nombre,titulo,contenido) VALUES ('Tirsio Gomora','Infor
 -- INSERT INTO public.empleados(nombre, apellido,depto, salario) VALUES('Jacinto','Gutierrez Delarde',3,24000.0);
 -- INSERT INTO public.empleados(nombre, apellido,depto, salario) VALUES('Adriana','Matamoros Mori',4,23000.0);
 
+
 -- select notas.titulo, notas.nombre as autor,notas.contenido, now() as fecha  from public.notas as notas order by notas.nombre;
 -- select notas.titulo, notas.nombre as autor,notas.contenido, now() as fecha  from public.notas as notas;
