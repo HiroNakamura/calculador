0f40a7bf6d51729ee0235e1fa6acc204f38af35b
ARIEL <carraro.ariel@gmail.com>
2019-08-09 12:15:56 -0500

    Se edito import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,7 @@
-
-
 INSERT INTO public.notas(nombre,titulo,contenido) VALUES ('Umberto Eco','Sobre la esquizofrenia natural','Blah blah y blah');
 INSERT INTO public.notas(nombre,titulo,contenido) VALUES ('Inmanuel Kant','Tratado de logica distopica','En muchas palabras...');
 INSERT INTO public.notas(nombre,titulo,contenido) VALUES ('Hortencia Alcantara','Hablando de los sentimientos humanos','Creo que no escribire de esto');
 INSERT INTO public.notas(nombre,titulo,contenido) VALUES ('Tirsio Gomora','Informe sobre ciegos','Y exponiendo el asunto tenemos que las gallinas ponen muchos huevos');
-
-
 
 -- INSERT INTO public.empleados(nombre, apellido,depto, salario) VALUES('Juan','Lopez Lopez',1,23000.0);
 -- INSERT INTO public.empleados(nombre, apellido,depto, salario) VALUES('Mariana','Alcantara Villegas',2,25000.0);
@@ -15,3 +11,6 @@ INSERT INTO public.notas(nombre,titulo,contenido) VALUES ('Tirsio Gomora','Infor
 -- INSERT INTO public.empleados(nombre, apellido,depto, salario) VALUES('Yuridiana','Lopez Valdez',1,23000.0);
 -- INSERT INTO public.empleados(nombre, apellido,depto, salario) VALUES('Jacinto','Gutierrez Delarde',3,24000.0);
 -- INSERT INTO public.empleados(nombre, apellido,depto, salario) VALUES('Adriana','Matamoros Mori',4,23000.0);
+
+-- select notas.titulo, notas.nombre as autor,notas.contenido, now() as fecha  from public.notas as notas order by notas.nombre;
+-- select notas.titulo, notas.nombre as autor,notas.contenido, now() as fecha  from public.notas as notas;
