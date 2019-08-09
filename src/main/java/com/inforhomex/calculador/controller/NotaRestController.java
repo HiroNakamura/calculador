55cf58641c1abe807694b1fd5985b44bf009c1c5
Fer_Carraro <carraro.fernando@gmail.com>
2019-08-09 17:22:12 -0500

    Se edito NotasRestController

--- src/main/java/com/inforhomex/calculador/controller/NotaRestController.java
+++ src/main/java/com/inforhomex/calculador/controller/NotaRestController.java
@@ -13,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.dao.DataAccessException;
 
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
 
 @CrossOrigin("*")
 @RestController
@@ -33,8 +37,27 @@ public class NotaRestController{
     
     //http://localhost:8090/curso/notas/1
     @GetMapping("/notas/{id}")
-    public MNota nota(@PathVariable Long id){
-        return notaServiceImpl.findNotaById(id);
+    public ResponseEntity<?> nota(@PathVariable Long id){
+        
+        MNota mnota = null;
+        Map<String,Object> response = new HashMap<>();
+
+        try {
+            mnota = notaServiceImpl.findNotaById(id);
+            response.put("mnota",mnota);
+            response.put("mensaje","Obtenido exitosamente: "+mnota);
+        } catch(DataAccessException e) {
+            response.put("mensaje", "Error al realizar la consulta en la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
+        if(mnota == null) {
+            response.put("mensaje", "La nota con ID: ".concat(id.toString().concat(" no existe en la base de datos!")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Map<String,Object>>(response, HttpStatus.OK);
     }
 
     
