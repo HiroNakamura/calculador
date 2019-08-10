aa8be05990803c86a2a80ea7b79ee0a989c222f4
Fer_Carraro <carraro.fernando@gmail.com>
2019-08-09 22:26:48 -0500

    Se crearon nuevas etidades

---
+++ src/main/java/com/inforhomex/calculador/entity/Autor.java
@@ -0,0 +1,22 @@
+package com.inforhomex.calculador.entity;
+
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+
+public class Autor implements Serializable{
+
+	private static final long serialVersionUID = 25L;
+
+	public Autor(){}
+
+
+
+	
+}
