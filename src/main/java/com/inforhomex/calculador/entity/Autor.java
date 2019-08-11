4f05f7bde0f64ec995f90cf65cb613b9c73b337e
ARIEL <carraro.ariel@gmail.com>
2019-08-11 16:38:08 -0500

    Se edito entidad Autor

--- src/main/java/com/inforhomex/calculador/entity/Autor.java
+++ src/main/java/com/inforhomex/calculador/entity/Autor.java
@@ -23,7 +23,7 @@ public class Autor implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(columnDefinition = "serial")
+	@Column(columnDefinition = "serial")
 	private Long id;
 
 	private String nombre;
