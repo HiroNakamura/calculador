e135e4461220865ddad6575e1ef76ef99ee079ef
Fer_Carraro <carraro.fernando@gmail.com>
2019-08-12 18:00:07 -0500

    Se argregaron cambios en Libro repositorio

--- src/main/java/com/inforhomex/calculador/repository/LibroRepository.java
+++ src/main/java/com/inforhomex/calculador/repository/LibroRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 
 @Repository("libroRepository")
 public interface LibroRepository extends JpaRepository<Libro,Long>{
-    @Query(value="SELECT autor.id, autor.nombre,autor.apellido, libro.titulo FROM public.libros AS libro INNER JOIN public.autores AS autor ON autor.id=libro.autor_id order by autor.nombre",nativeQuery=true)
+    @Query(value="SELECT * FROM public.libros",nativeQuery=true)
     public List<Libro> getAllLibros();
 
     @Query(value="SELECT * FROM public.libros AS libro WHERE libro.id = ?1",nativeQuery=true)
