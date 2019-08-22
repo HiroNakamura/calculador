459901ff1ebf3ff865f5cb0b33947cdd080d8e8b
Fer_Carraro <carraro.fernando@gmail.com>
2019-08-22 15:51:06 -0500

    Se editaron varios archivos

--- src/main/java/com/inforhomex/calculador/controller/CursoController.java
+++ src/main/java/com/inforhomex/calculador/controller/CursoController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,11 +22,15 @@ public class CursoController{
     @Value("${programador.aplicacion}")
     private String nombre;
 
+    @Value("#{new java.text.SimpleDateFormat('${programador.fechaFormat}').parse('${programador.fecha}')}")
+    private Date fecha;
+
     //http://localhost:8090/calculador/index
     @GetMapping("/index")
     public String index(Model model){
         LOG.info("Has entrado a http://localhost:8090/calculador/index");
         model.addAttribute("nombre", nombre);
+        model.addAttribute("fecha", fecha);
         return "index";
     }
 
