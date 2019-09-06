fe5775e6d17ba93c646432b00bbfac003e9b3147
Fer_Carraro <carraro.fernando@gmail.com>
2019-09-06 11:22:30 -0500

    Se edito QuijoteFrases

--- src/main/java/com/inforhomex/calculador/model/QuijoteFrases.java
+++ src/main/java/com/inforhomex/calculador/model/QuijoteFrases.java
@@ -3,7 +3,9 @@ package com.inforhomex.calculador.model;
 
 public final class QuijoteFrases{
 
-	public static final String[] FRASE = {"A menos que sea la muerte, para todo hay remedio.".toUpperCase()
+	public static final String[] FRASE = {"a menos que sea la muerte, para todo hay remedio.".toUpperCase()
+	,"da crédito a las obras y no a las palabras".toUpperCase()
+	,"hacer bien a villanos es echar agua en el mar".toUpperCase()
 	,"no puede impedirse el viento, pero hay que saber hacer molinos".toUpperCase()
 	,"si los perros ladran, Sancho, es señal que cabalgamos".toUpperCase()};
 	
@@ -11,7 +13,7 @@ public final class QuijoteFrases{
 
 	
 	public String getFrases(){
-		return FRASE[(int)Math.random()*2];
+		return FRASE[(int)Math.random()*4];
 	}
 
 }
